084acfa4527ec9dc3bf58fe193ef06b29d46d60d
Paul Bester <paul.bester@outlook.com>
2023-07-01 22:06:11 +0100

    First bestedev-api

--- src/main/java/ie/bester/besterdevapi/controller/ControllerOne.java
+++ src/main/java/ie/bester/besterdevapi/controller/ControllerOne.java
@@ -3,6 +3,7 @@ package ie.bester.besterdevapi.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+;
 import ie.bester.besterdevapi.entity.EntityOne ;
 import ie.bester.besterdevapi.repository.RepoOne;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,8 +28,8 @@ public class ControllerOne {
     private String helloWorld() {
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy.MM.dd @ HH:mm");
         LocalDateTime now = LocalDateTime.now();
-//        System.out.println(dtf.format(now));
         return "Hello World......" +now;}
+
 
     //RETURN ALL RECORDS:
     //PROD: https://besterdev-be-f61a2efc23c9.herokuapp.com/api/v1/records
@@ -36,7 +37,8 @@ public class ControllerOne {
     @GetMapping("/records")
     private List<EntityOne> getAllRecords() {return repoOne.findAll();}
 
-    //CREATE NEW RECORD:
+
+    //CREATE NEW RECORD (POST):
     //PROD: https://besterdev-be-f61a2efc23c9.herokuapp.com/api/v1/records/create
     //LOCAL: http://localhost:8000/api/v1/records/create
     //POST STRING: { "colone": "1", "coltwo": "2", "colthree": "3", "colfour": "4", "coldate": "2023-02-02"}
@@ -45,4 +47,22 @@ public class ControllerOne {
         EntityOne newRecord = repoOne.save(tableoneentity);
         return newRecord;}
 
+
+    //UPDATE RECORD BY ID (PUT):
+    //PROD: https://besterdev-be-f61a2efc23c9.herokuapp.com/api/v1/records/update/{ID}
+    //LOCAL: http://localhost:8000/api/v1/records/update/{ID}
+    //PUT STRING: { "colone": "PUT", "coltwo": "PUT", "colthree": "PUT", "colfour": "PUT", "coldate": "2000-01-01"}
+    @RequestMapping(value = "/records/update/{ID}", method = RequestMethod.PUT)
+    ResponseEntity<EntityOne> update (@PathVariable int ID, @RequestBody EntityOne changes) {
+        System.out.println("Update a Record by ID");
+        Optional<EntityOne> optionalChange = repoOne.findById(ID);
+            {EntityOne existingChange = optionalChange.get();
+            existingChange.setcolone(changes.getcolone());
+            existingChange.setcoltwo(changes.getcoltwo());
+            existingChange.setcolthree(changes.getcolthree());
+            existingChange.setcolfour(changes.getcolfour());
+            existingChange.setcoldate(changes.getcoldate());
+            EntityOne savedChange = repoOne.save(existingChange);
+            return new ResponseEntity<EntityOne>(savedChange, HttpStatus.ACCEPTED);}
+    }
 }
--- src/main/java/ie/bester/besterdevapi/repository/RepoOne.java
+++ src/main/java/ie/bester/besterdevapi/repository/RepoOne.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Optional;
 public interface RepoOne extends JpaRepository<EntityOne, Integer>{
-    public List<Optional<EntityOne>> findById (int ID);
+    public Optional<EntityOne> findById (int ID);
+
 }
